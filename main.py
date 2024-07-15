66189cd967bd8c3720fbd3697672d34fbcb18a75
Tauri StClaire <104536950+tauri-st@users.noreply.github.com>
2024-07-15 22:36:49 +0000

    Add the flatten function

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ with open("titanic.csv", "r") as file:
   data = list(data)
   data = np.array(data)
   
-#create a numpy array with a slice for the survived column which is in index position zero and we want the data to be an integer
-survived = np.array(data[:,[0]], dtype=int)
+#create a numpy array with a slice for the survived column which is in index position zero and we want the data to be an integer. 
+#The flatten function turns the array from two-dimensional to one which is easier to work with
+survived = np.array(data[:,[0]], dtype=int).flatten()
 fare = np.array
 
 #empty lists to hold data from  the for loop
